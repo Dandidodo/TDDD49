47cd1372c5ac9a71c1d4e7360e4f5c8039413a6d
unknown <marlu642@PC3-7.helix.ida.liu.se>
2016-11-03 11:56:16 +0100

    Add project

---
+++ PokerTest/CardTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Poker;
+
+namespace PokerTest
+{
+    [TestClass]
+    public class CardTest
+    {
+        [TestMethod]
+        public void CreateCard()
+        {
+            Card card = new Card(Card.Suit.Club, 2);
+
+            Assert.AreEqual(Card.Suit.Club, card.getSuit());
+        }
+    }
+}
