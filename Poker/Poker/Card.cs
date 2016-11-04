76bdccbe1e6d6abc051569fc65743f3f63c23f24
Pierre Hedkvist <piehe154@student.liu.se>
2016-11-04 11:16:31 +0100

    Add wpf project, and class for table and texas holdem rules

---
+++ Poker/Poker/Card.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker
+{
+    public class Card
+    {
+        private int rank;
+        private Suit suit;
+
+        public enum Suit { Spade, Heart, Diamond, Club };
+
+
+        public Card(Suit suit, int rank)
+        {
+            this.suit = suit;
+            this.rank = rank;
+        }
+
+
+        public Suit getSuit()
+        {
+            return suit;
+        }
+
+        public int getRank()
+        {
+            return rank;
+        }
+
+        public void setRank(int rank)
+        {
+            //TODO: Validera att rank är korrekt, 2<=rank<=14
+            this.rank = rank;
+        }
+
+        public void setSuit(Suit suit)
+        {
+            this.suit = suit;
+        }
+    }
+}
---
+++ Poker/Poker/Deck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker
+{
+    class Deck
+    {
+        List<Card> cards = new List<Card>();
+
+        public Deck()
+        {
+            var suits = Enum.GetValues(typeof(Card.Suit));
+
+            foreach (Card.Suit suit in suits)
+            {
+                for (int i = 2; i < 15; i++)
+                {
+                    cards.Add(new Card(suit, i));
+                }
+            }
+        }
+
+
+    }
+}
