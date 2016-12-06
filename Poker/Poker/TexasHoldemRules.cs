bf773bd90055d5872c1381da93ece280e0d583f7
Martin Lundberg <dandidodo@hotmail.com>
2016-12-06 14:18:21 +0100

    Removed gitignore and added all the latest code.

--- Poker/Poker/TexasHoldemRules.cs
+++ Poker/Poker/TexasHoldemRules.cs
@@ -26,7 +26,6 @@ namespace Poker
         private List<Player_entity> players;
         private Deck deck;
         private Table_entity table;
-        private int pot;
         private int indexBigBlind;
         private int indexSmallBlind;
         private int roundCounter;
@@ -115,8 +114,6 @@ namespace Poker
             currentPlayer = (indexBigBlind == players.Count - 1) ? players[0] : players[indexBigBlind + 1];
             
             dealCards();
-
-            //playerAction(currentPlayer);
         }
 
         public void changeBlindIndexes()
@@ -184,31 +181,7 @@ namespace Poker
             for (int i = 0; i < 5; i++)
                 table.setCM(deck.draw());
         }
-
-        public void playerAction()
-        {
-            //1) Fold, 2) Call, 3) Raise
-            // Show graphical options to current player
-
-
-            /* 
-            while(true)
-            {             
-               if (playerChoice == Choice.FOLD)
-               {
-
-               } else if (playerChoice == Choice.CHECK)
-               {
-
-               } else if (playerChoice == Choice.RAISE)
-               {
-
-               }           
-            }
-            */
-
-        }
-
+        
         // function for fold
         public void playerFold()
         {
@@ -218,7 +191,7 @@ namespace Poker
             currentPlayer.setStakes(0);
 
             // Get next player
-            Player_entity nextPlayer = getNextPlayer();
+            Player_entity nextPlayer = getNextActivePlayer(currentPlayer);
 
             // Remove player from active players
             currentPlayer.Active = false;
@@ -250,19 +223,13 @@ namespace Poker
             // Check if player is out of chips
             // NextPlayer
             // Get next player
-            Player_entity nextPlayer = getNextPlayer();
+            Player_entity nextPlayer = getNextActivePlayer(currentPlayer);
 
             currentPlayer = nextPlayer;
 
             if (roundIsFinished())
             {
-                foreach (Player_entity player in players)
-                {
-                    player.ActedThisRound = false;
-                    table.setPot(table.getPot() + player.getStakes());
-                    player.setStakes(0);
-                }
-                currentPlayer = players[indexSmallBlind];
+                newRound();
 
                 // River
                 if (roundCounter == 3)
@@ -291,7 +258,7 @@ namespace Poker
             // TODO: Check if player is out of chips
             
             // NextPlayer
-            Player_entity nextPlayer = getNextPlayer();
+            Player_entity nextPlayer = getNextActivePlayer(currentPlayer);
 
             currentPlayer = nextPlayer;
         }     
@@ -299,33 +266,49 @@ namespace Poker
         // TODO: Move this to a better suited place
         // Find the next player, we have to check a special case if the current active player is in the end of players, then we have to
         // return the first active player found.
-        public Player_entity getNextPlayer()
+        public Player_entity getNextActivePlayer(Player_entity player)
         {
             Player_entity nextPlayer = new Player_entity();
-            bool currentPlayerFound = false;
+            bool playerFound = false;
             bool firstActivePlayerFound = false;
 
-            foreach (Player_entity player in players)
+            foreach (Player_entity p in players)
             {
-                if (player == currentPlayer)
+                if (p == player)
                 {
-                    currentPlayerFound = true;
+                    playerFound = true;
                 }
-                else if (currentPlayerFound == true)
+                else if (playerFound == true)
                 {
-                    return player;
+                    return p;
                 }
-                else if (player.Active == true && !firstActivePlayerFound)
+                else if (p.Active == true && !firstActivePlayerFound)
                 {
-                    nextPlayer = player;
+                    nextPlayer = p;
                     firstActivePlayerFound = true;
                 }                
             }
             return nextPlayer;
         }
 
+        public void newRound()
+        {
+            foreach (Player_entity player in players)
+            {
+                player.ActedThisRound = false;
+                table.setPot(table.getPot() + player.getStakes());
+                player.setStakes(0);
+            }
+            currentPlayer = getNextActivePlayer(players[indexSmallBlind - 1]); // Will not work when indexSmallBlind == 0
+            lastRaise = 0;
+            minRaise = bigBlind;
+        }
+
+        // Rank each hand from 1-10 from best to worst, also rank the strenght of the hand,
+        // say two players have a pair, which one is the strongest.
         private void compareHands()
         {
+            /*
             foreach (Player_entity player in players)
             {
                 if (player.Active)
@@ -335,42 +318,136 @@ namespace Poker
                     List<Card_entity> allCards = new List<Card_entity>(table.getCommunityCards());
                     allCards.Add(player.getCards()[0]);
                     allCards.Add(player.getCards()[1]);
-
                     checkSameRank(allCards);
+                    checkFlush(allCards);
+                    checkStraight(allCards);
                 }
-            }
+            }*/
+
+            //Fake shit for testing purposes only
+            List<Card_entity> testStraight = new List<Card_entity>();
+            testStraight.Add(new Card_entity(Card_entity.Suit.Spade, 1));
+            testStraight.Add(new Card_entity(Card_entity.Suit.Club, 4));
+            testStraight.Add(new Card_entity(Card_entity.Suit.Spade, 5));
+            testStraight.Add(new Card_entity(Card_entity.Suit.Spade, 6));
+            testStraight.Add(new Card_entity(Card_entity.Suit.Club, 7));
+            testStraight.Add(new Card_entity(Card_entity.Suit.Club, 8));
+            testStraight.Add(new Card_entity(Card_entity.Suit.Club, 2));
+
+            List<Card_entity> sortedCards = testStraight.OrderBy(c => c.getRank()).ToList();
+
+            checkFlush(sortedCards);
         }
 
 
-        private void checkSameRank(List<Card_entity> allCards)
+        private void checkSameRank(List<Card_entity> combinedCards)
         {
             Dictionary<int, int> rankCounter = new Dictionary<int, int>();
 
-            for (int card1_index = 0; card1_index < allCards.Count; card1_index++)
+            //Detta är en dum lösning, vi kan loopa en gång och incrementa samma siffra om den redan finns....
+            for (int card1_index = 0; card1_index < combinedCards.Count; card1_index++)
             {
-                for (int card2_index = card1_index + 1; card2_index < allCards.Count; card2_index++)
+                for (int card2_index = card1_index + 1; card2_index < combinedCards.Count; card2_index++)
                 {
-                    Card_entity card1 = allCards[card1_index];
-                    Card_entity card2 = allCards[card2_index];
+                    Card_entity card1 = combinedCards[card1_index];
+                    Card_entity card2 = combinedCards[card2_index];
 
-                    if (card1!= card2)
+                    if (card1.getRank() == card2.getRank())
                     {
-                        if (card1.getRank() == card2.getRank())
+                        if (rankCounter.ContainsKey(card1.getRank()))
                         {
-                            if (rankCounter.ContainsKey(card1.getRank()))
-                            {
-                                int value = rankCounter[card1.getRank()];
-                                rankCounter[card1.getRank()] = value + 1; //Something more than a pair, three of a kind etc...
-                            }
-                            else
-                            {
-                                rankCounter.Add(card1.getRank(), 1); //We found a pair
-                            }
+                            int value = rankCounter[card1.getRank()];
+                            rankCounter[card1.getRank()] = value + 1; //Something more than a pair, three of a kind etc...
+                        }
+                        else
+                        {
+                            rankCounter.Add(card1.getRank(), 1); //We found a pair
                         }
                     }
                 }
             }
+
+            int[,] three_of_a_kind;
+            int[,] pair;
+
+            foreach (KeyValuePair<int, int> rank in rankCounter)
+            {
+                if (rank.Value == 4)
+                {
+                    //returnera direkt med four of a kind, har två spelare samma foak (dv four of a kind på community cards), så blir avgörande highest kicker, om olika foak så blir den med högsta foak.
+                }
+                else if (rank.Value == 3)
+                {
+                    //save in a variable, we might have a full house
+                }
+                else
+                {
+                    //save the pair because
+                }
+            }
+
+            //If still here we have to return what we found.
+
             Console.WriteLine(rankCounter);
+        }
+
+        /*
+        private int[,] evalHand(Dictionary<int,int> rankCounter)
+        {
+        }*/
+
+        // Returns if any suit occures 5 times or more.
+        private bool checkFlush(List<Card_entity> combinedCards)
+        {
+            int heartCount = 0;
+            int spadesCount = 0;
+            int diamondCount = 0;
+            int clubCount = 0;
+
+
+            for (int i = 0; i < combinedCards.Count; i++)
+            {
+                Card_entity.Suit currentSuit = combinedCards[i].getSuit();
+
+                if (currentSuit == Card_entity.Suit.Heart)
+                    heartCount++;
+                else if (currentSuit == Card_entity.Suit.Spade)
+                    spadesCount++;
+                else if (currentSuit == Card_entity.Suit.Diamond)
+                    diamondCount++;
+                else
+                    clubCount++;
+            }
+
+            return ((heartCount >= 5) || (spadesCount >= 5) || (diamondCount >= 5) || (clubCount >= 5));
+        }
+
+        private bool checkStraight(List<Card_entity> combinedCards)
+        {
+
+
+            int previousRank = combinedCards[0].getRank();
+            int currentStreak = 0; // The cards rank has to increase five times, but we dont know where the straight starts.
+
+            for (int i = 1; i < combinedCards.Count; i++)
+            {
+                int currentRank = combinedCards[i].getRank();
+                if ((previousRank + 1) == currentRank)
+                {
+                    currentStreak++;
+                    if (currentStreak == 4)
+                        //TODO: Check if last card is Ace -> Set royal flag
+                        return true; //Here we have found it
+                }
+                else
+                {
+                    currentStreak = 0;
+                }
+
+                previousRank = currentRank;
+
+            }
+            return false;
         }
     }
 }
